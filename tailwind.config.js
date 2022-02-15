8488995d781d8eb04f9a44f55c4ab50fd1d2f12c
Alexander Ming <alexanderming@AlexandrtekiMBP.fios-router.home>
2022-02-15 05:33:58 -0500

    set up a layout, navigate all the pages.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   content: [],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
